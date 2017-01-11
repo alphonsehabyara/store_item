709df14efd617d729a70b5df23452192f565dc1d
Alphonse Habyarimana <habyara.malpha@gmail.com>
2017-01-11 14:51:14 +0200

    currency define for items

--- store_item.rb
+++ store_item.rb
@@ -1,5 +1,5 @@
-p_shoes1 = {"type" => "Almond Toes", "color" => "black & white", "price" => 100}
-p_shoes2 = {"type" => "Driving Mocs", "color" => "silver", "price" => 150}
-p_shoes3 = {"type" => "Sandals", "color" => "mixt of gree and yellow", "price" => 80}
+p_shoes1 = {"type" => "Almond Toes", "color" => "black & white", "price" => "$100"}
+p_shoes2 = {"type" => "Driving Mocs", "color" => "silver", "price" => "$150"}
+p_shoes3 = {"type" => "Sandals", "color" => "mixt of green and yellow", "price" => "$80"}
 
-puts "Available items are #{p_shoes2["type"]} with the color of #{p_shoes2["color"]} and they cost $#{p_shoes2["price"]} each pair!"
+puts "Available items are #{p_shoes2["type"]} with the color of #{p_shoes2["color"]} and they cost #{p_shoes2["price"]} each pair!"
