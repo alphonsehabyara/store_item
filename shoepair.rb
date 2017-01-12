f3a8ac99a0fe667dafa766d318d81a402ffebb74
Alphonse Habyarimana <habyara.malpha@gmail.com>
2017-01-12 12:42:24 +0200

    used hashes to make the codes easier to read

--- shoepair.rb
+++ shoepair.rb
@@ -3,22 +3,22 @@ class ShoePair
 	attr_reader :type, :color, :price
 	attr_writer :price
 	
-	def initialize(type, color, price)
-		@type = type 
-		@color = color 
-		@price = price
+	def initialize(shoe_hash)
+		@type = shoe_hash[:type] 
+		@color = shoe_hash[:color]
+		@price = shoe_hash[:price]
 	end 
 	def info
-		"#{@type} of #{@color} color at $#{@price}"
+		"#{@type} of #{color} color at $#{price}"
 	end
 end
-shoepair1 = ShoePair.new("Almond Toes", "black & white", 100)
-shoepair2 = ShoePair.new("Driving Mocs", "silver", 80)
-shoepair3 = ShoePair.new("Sandals", "mixt of green and yellow", 150)
+shoepair1 = ShoePair.new({type: "Almond Toes", color: "black & white", price: 100})
+shoepair2 = ShoePair.new({type: "Driving Mocs", color: "silver", price: 80})
+shoepair3 = ShoePair.new({type: "Sandals", color: "mixt of green and yellow", price: 150})
 
-shoepair1.price = 200
+
 puts shoepair1.price
+shoepair1.price = 200
 puts shoepair1.info
 puts shoepair2.info
 puts shoepair3.info
-
